17525d9fdf7730614280406ff7d09671d900d7b5
GabriellyPires <lazulig29@gmail.com>
2024-10-14 07:56:53 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+//console.log(“Hello world”);
+//let num1 = 15;
+//let num2 = 27;  
+console.log('Conexão feita com sucesso!');
+console.log('Conexão feita com sucesso!');
+console.log('Conexão feita com sucesso!');
